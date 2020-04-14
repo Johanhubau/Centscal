935292cfe6c46888b8b3578cbf7ac890aee3ea12
Johan Hubau <johan.hubau@gmail.com>
2020-04-14 14:09:13 +0200

    Make some tests
    More specifically, only the user tests that were the basis to understand how they work and how to do the rest of the tests.
    
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+//USER ROUTES
+Route::post('/user', 'UserController@store');
+Route::get('/user/{user}', 'UserController@show');
+Route::post('/user/{user}', 'UserController@update');
+Route::delete('/user/{user}', 'UserController@destroy');
