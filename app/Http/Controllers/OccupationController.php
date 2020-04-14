3e5db09931367baf339f3e50607378fbf6ee8b35
Johan Hubau <johan.hubau@gmail.com>
2020-04-14 11:30:17 +0200

    Add missing controllers
    Signed-off-by: Johan Hubau <johan.hubau@gmail.com>

---
+++ app/Http/Controllers/OccupationController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Occupation;
+use Illuminate\Http\Request;
+
+class OccupationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Occupation[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return Occupation::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+           'room_id' => 'required|exists:rooms,id',
+           'event_id' => 'required|exists:events,id',
+           'approved' => 'boolean'
+        ]);
+
+        Occupation::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Occupation $occupation
+     * @return Occupation
+     */
+    public function show(Occupation $occupation)
+    {
+        return $occupation;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Occupation $occupation)
+    {
+        $validated = $request->validate([
+           'approved' => 'required|boolean'
+        ]);
+
+        $occupation->update($validated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Occupation $occupation
+     * @return void
+     * @throws \Exception
+     */
+    public function destroy(Occupation $occupation)
+    {
+        $occupation->delete();
+    }
+}
---
+++ app/Http/Controllers/RentController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rent;
+use Illuminate\Http\Request;
+
+class RentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Rent[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return Rent::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+           'event_id' => 'required|exists:events,id',
+           'material_id' => 'required|exists:materials,id',
+            'approved' => 'boolean'
+        ]);
+
+        Rent::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Rent $rent
+     * @return Rent
+     */
+    public function show(Rent $rent)
+    {
+        return $rent;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param Rent $rent
+     * @return void
+     */
+    public function update(Request $request, Rent $rent)
+    {
+        $validated = $request->validate([
+           'approved' => 'required|boolean'
+        ]);
+
+        $rent->update($validated);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Rent $rent
+     * @return void
+     * @throws \Exception
+     */
+    public function destroy(Rent $rent)
+    {
+        $rent->delete();
+    }
+}
